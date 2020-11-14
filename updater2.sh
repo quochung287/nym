8595503b8a013fb8c9aa791cd7fa6fb55fe8f49a
quochung287 <74443840+quochung287@users.noreply.github.com>
2020-11-14 23:16:44 +0700

    Add files via upload

--- updater2.sh
+++ updater2.sh
@@ -46,7 +46,7 @@ cd /home/nym2
 #printf "%b\n\n\n" "${WHITE} You selected ${YELLOW} $directory"
 sleep 2
 printf "%b\n\n\n" "${WHITE} Enter the Liquid-BTC address for the incentives rewards"
-vireward=(VJLBvi1r9vnmEZAhxVk4MsdihAecfhw7yhF78r1XjYX1Ahc1fb2jzVLbQKnBSrjsFZpwpujkSRCcj3ic)
+vireward=(VJLCyUN19M3oBBXhNVs3U8EThrgHwGAZHbC1oNCBKyJ1sBz5hdqSHhpnbQhkc3WNPU1WjtZTS1GSLVo4)
 printf "%b\n\n\n"
 printf "%b\n\n\n" "${WHITE} Address for the incentives rewards will be ${YELLOW} ${vireward} "
 printf "%b\n\n\n" "${WHITE} You may later change it in config.toml if needed, but you need to stop the node first and then edit it with an editor such as nano"
