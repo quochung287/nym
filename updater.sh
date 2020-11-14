9dbebfa13c4798ae2025228be53c9b39c2524710
quochung287 <74443840+quochung287@users.noreply.github.com>
2020-11-14 16:56:11 +0700

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -46,7 +46,7 @@ printf "%b\n\n\n"
 printf "%b\n\n\n" "${WHITE} You selected ${YELLOW} $directory"
 sleep 2
 printf "%b\n\n\n" "${WHITE} Enter the Liquid-BTC address for the incentives rewards"
-vireward=(VJLDsBLDpCsJHaYvLqQ17961iN6JrwupqAhp5Gy9ZbCEjGT9AGwoso32AmdF66myKU2gcX93xBM56Cij)
+vireward=(VJLF5MyZ9QGHQf2DxTpj4M9GrGT4TaKhNhSdU7ntjrHUGtLb8Nnnnd3XydkamWneLpTN1XdXwBkQVFca)
 printf "%b\n\n\n"
 printf "%b\n\n\n" "${WHITE} Address for the incentives rewards will be ${YELLOW} ${vireward} "
 printf "%b\n\n\n" "${WHITE} You may later change it in config.toml if needed, but you need to stop the node first and then edit it with an editor such as nano"
